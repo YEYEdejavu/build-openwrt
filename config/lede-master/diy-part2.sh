5af1e46af4749521ceb872f7e2efa1f69be45817
YEYEdejavu <129372653+YEYEdejavu@users.noreply.github.com>
2024-03-02 00:28:51 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -28,9 +28,9 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 
 # ------------------------------- Other started -------------------------------
 #
-# Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-
+# Add 
+svn co https://github.com/EOYOHOO/UA2F.git package/UA2F
+svn co https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
 # svn export https://github.com/openwrt/packages/trunk/utils/runc/Makefile ./feeds/packages/utils/runc/Makefile
